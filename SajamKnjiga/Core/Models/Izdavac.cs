672d665c638ed5c78d19dad315a9d1eb38cca313
Milica <milicastankovvicc@gmail.com>
2025-12-22 18:11:18 +0100

    renew izdavac, kupovina, posetilac

---
+++ SajamKnjiga/Core/Models/Izdavac.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    internal class Izdavac
+    {
+
+        private int sifra;
+        private string naziv;
+        private Autor sef;
+        private List<Autor> spisakAutora;
+        private List<Autor> spisakKnjiga;
+
+        public int Sifra
+        {
+            get { return sifra; }
+            set { sifra = value; }
+        }
+
+        public string Naziv
+        {
+            get { return naziv; }
+            set { naziv = value; }
+        }
+
+        public Autor Sef
+        {
+            get { return sef; }
+            set
+            {
+                if (value.GodineIskustva < 5)
+                    throw new ArgumentOutOfRangeException("Sef mora imati najmanje 5 godina iskustva.");
+                sef = value;
+            }
+        }
+
+        public List<Autor> SpisakAutora { get; set; }
+        public List<Autor> SpisakKnjiga { get; set; }
+
+        public Izdavac()
+        {
+
+        }
+
+        public Izdavac(int sifra, string naziv, Autor sef)
+        {
+            Sifra = sifra;
+            Naziv = naziv;
+            Sef = sef;   //seter proverava godine iskustva
+            SpisakAutora = new List<Autor>();
+            SpisakKnjiga = new List<Knjiga>();
+        }
+
+
+    }
+}
