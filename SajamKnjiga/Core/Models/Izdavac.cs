718aaf0b3821e90d37dbb00e8d5a9846f4435212
Milica <milicastankovvicc@gmail.com>
2025-12-22 13:28:54 +0100

    Add basic structure for Izdavac, Kupovina and Posetilac classes

---
+++ SajamKnjiga/Core/Models/Izdavac.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+
+public class Izdavac
+{
+    private int sifra;
+    private string naziv;
+    private Autor sef;
+    private List<Autor> spisakAutora;
+    private List<Autor> spisakKnjiga;
+
+    public int Sifra
+    {
+        get { return sifra; }
+        set { sifra = value; }
+    }
+
+    public string Naziv
+    {
+        get { return naziv; }
+        set { naziv = value; }
+    }
+
+    public Autor Sef
+    {
+        get { return sef; }
+        set {
+            if (value.GodineIskustva < 5)
+                throw new ArgumentOutOfRangeException("Sef mora imati najmanje 5 godina iskustva.");
+            sef = value; }
+    }
+
+    public List<Autor> SpisakAutora { get; set; }
+    public List<Autor> SpisakKnjiga { get; set; }
+
+    public Izdavac(int sifra, string naziv, Autor sef)
+    {
+        Sifra = sifra;
+        Naziv = naziv;
+        Sef = sef;   //seter proverava godine iskustva
+        SpisakAutora = new List<Autor>();
+        SpisakKnjiga = new List<Knjiga>();
+    }
+
+}
---
+++ SajamKnjiga/Core/Models/Kupovina.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+
+public class Kupovina
+{
+	private Posetilac kupac;
+	private Knjiga knjiga;
+	private DateTime datumKupovine;
+	private double ocena;
+	private string komentar;
+
+	public Posetilac Kupac
+	{
+		get { return kupac; }
+		set { kupac = value; }
+	}
+
+	public Knjiga Knjiga
+	{
+		get { return knjiga; }
+		set { knjiga = value; }
+	}
+
+	public DateTime DatumKupovine
+	{
+		get { return datumKupovine; }
+		set { datumKupovine = value; }
+	}
+
+	public double Ocena
+	{
+		get { return ocena; }
+		set
+		{
+			if (value < 1 || value > 5)
+				throw new ArgumentOutOfRangeException(nameof(value), "Ocena mora biti izmedju 1 i 5.");
+			ocena = value;
+		}
+	}
+
+	public string Komentar
+	{
+		get { return komentar; }
+		set { komentar = value; }
+	}
+
+	public Kupovina(Posetilac kupac, Knjiga knjiga, DateTime datum,
+		double ocena, string komentar)
+	{
+		Kupac = kupac;
+		Knjiga = knjiga;
+		DatumKupovine = datum;
+		Ocena = ocena;
+		Komentar = komentar;
+	}
+}
