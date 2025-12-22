a925b69d2b726600715582f0fc58a359473686d1
Stefan Karac <empytytv@gmail.com>
2025-12-22 18:49:22 +0100

    Added interfaces IAutorDao and IKnjigaDao

---
+++ SajamKnjiga/Core/Interfaces/IAutorDao.cs
@@ -0,0 +1,18 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    internal interface IAutorDao
+    {
+        void Add(Autor autor);
+        void Update(Autor autor);
+        void Delete(int sifra);
+        Autor GetBySifra(int sifra);
+        List<Autor> GetAll();
+    }
+}
---
+++ SajamKnjiga/Core/Interfaces/IKnjigaDao.cs
@@ -0,0 +1,18 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    internal interface IKnjigaDao
+    {
+        void addKnjiga(Knjiga knjiga);
+        void deleteKnjiga(string knjigaId);
+        void updateKnjiga(Knjiga knjiga);
+        List<Knjiga> getAllKnjige();
+        Knjiga GetKnjiga(string knjigaId);
+    }
+}
