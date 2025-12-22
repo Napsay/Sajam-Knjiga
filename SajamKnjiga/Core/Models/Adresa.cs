fbd5ca0bf36c7064ae050463c14df01a1669ef7b
Stefan Karac <empytytv@gmail.com>
2025-12-22 18:11:13 +0100

    Renew Adresa,Autor,Knjiga classes

---
+++ SajamKnjiga/Core/Models/Adresa.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    internal class Adresa
+    {
+        private string ulica;
+        private int broj;
+        private string grad;
+        private string drzava;
+
+        public string Drzava
+        {
+            get { return drzava; }
+            set { drzava = value; }
+        }
+        public string Grad
+        {
+            get { return grad; }
+            set { grad = value; }
+        }
+        public int Broj
+        {
+            get { return broj; }
+            set { broj = value; }
+        }
+        public string Ulica
+        {
+            get { return ulica; }
+            set { ulica = value; }
+        }
+
+        public Adresa(string ulica,int broj,string grad,string drzava)
+        {
+            Ulica = ulica;
+            Broj = broj;
+            Grad = grad;
+            Drzava = drzava;
+        }
+    }
+}
--- SajamKnjiga/Core/Models/Autor.cs
+++ SajamKnjiga/Core/Models/Autor.cs
@@ -8,5 +8,91 @@ namespace Core.Models
 {
     internal class Autor
     {
+        private int autorID;
+        private string ime;
+        private string prezime;
+        private DateTime datumRodjenja;
+        private string adresa;
+        private string telefon;
+        private string brojLicneKarte;
+        private int godineIskustva;
+        private string email;
+
+        public int AutorID
+        {
+            get { return autorID; }
+            set { autorID = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public List<Knjiga> Knjige { get; set; }
+        public int GodineIskustva
+        {
+            get { return godineIskustva; }
+            set { godineIskustva = value; }
+        }
+
+        public string BrojLicneKarte
+        {
+            get { return brojLicneKarte; }
+            set { brojLicneKarte = value; }
+        }
+
+        public string Telefon
+        {
+            get { return telefon; }
+            set { telefon = value; }
+        }
+
+        public string Adresa
+        {
+            get { return adresa; }
+            set { adresa = value; }
+        }
+
+        public DateTime DatumRodjenja
+        {
+            get { return datumRodjenja; }
+            set { datumRodjenja = value; }
+        }
+
+
+
+        public string Prezime
+        {
+            get { return prezime; }
+            set { prezime = value; }
+        }
+
+
+        public string Ime
+        {
+            get { return ime; }
+            set { ime = value; }
+        }
+        public Autor()
+        {
+            Knjige = new List<Knjiga>();
+        }
+        public Autor(int id,string ime, string prezime, DateTime datumRodjenja,
+                 string adresa, string telefon, string email,
+                 string brojLicneKarte, int godineIskustva)
+        {
+            AutorID = id;
+            Ime = ime;
+            Prezime = prezime;
+            DatumRodjenja = datumRodjenja;
+            Adresa = adresa;
+            Telefon = telefon;
+            Email = email;
+            BrojLicneKarte = brojLicneKarte;
+            GodineIskustva = godineIskustva;
+            Knjige = new List<Knjiga>();
+
+        }
     }
 }
